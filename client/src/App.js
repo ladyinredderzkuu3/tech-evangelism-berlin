95273dbdeea74230acced79e925ae3e1b8012daf
ladyinredderzkuu3 <mwineesref@hotmail.com>
2025-09-29 09:01:21 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tech Evangelism Berlin</h1>
+        <p>Willkommen in unserer Gemeinschaft!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
